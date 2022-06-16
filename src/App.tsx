455c2cf5cff3bfe3bf3e0f1e512b0bc252e5bb12
Trevor <trevorsm@pm.me>
2022-06-16 15:16:51 -0600

    added offset

--- src/App.tsx
+++ src/App.tsx
@@ -10,29 +10,31 @@ function App() {
 
     const today = '06/16/2022';
 
+    const offset = 3;
+
     draw.timeblock({
-      col: 2,
+      col: offset,
       color: '#98ffd9',
       startTime: new Date('06/15/2022 0:00:00'),
       endTime: new Date(today + ' 1:00:00'),
     });
 
     draw.timeblock({
-      col: 2,
+      col: offset,
       color: '#98b4ff',
       startTime: new Date(today + ' 8:00:00'),
       endTime: new Date(today + ' 12:00:00'),
     });
 
     draw.timeblock({
-      col: 3,
+      col: offset + 1,
       color: '#ff9c98',
       startTime: new Date(today + ' 9:00:00'),
       endTime: new Date(today + ' 13:20:00'),
     });
 
     draw.timeblock({
-      col: 2,
+      col: offset,
       color: '#98b4ff',
       startTime: new Date(today + ' 14:00:00'),
       endTime: null,
