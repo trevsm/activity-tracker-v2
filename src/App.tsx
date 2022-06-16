98f2b107d7f30ff90a75d6525388273563fe6291
Trevor <trevorsm@pm.me>
2022-06-15 23:24:52 -0600

    day refactor

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,33 @@ function App() {
     draw.bgGrid();
     draw.greyShroud();
 
+    const today = '06/15/2022';
+
     draw.timeblock({
       col: 1,
       color: '#9bff98',
       startTime: new Date('06/14/2022 17:00:00'),
-      endTime: new Date('06/15/2022 1:00:00'),
+      endTime: new Date(today + ' 1:00:00'),
     });
 
     draw.timeblock({
       col: 1,
       color: '#98b4ff',
-      startTime: new Date('06/15/2022 8:00:00'),
-      endTime: new Date('06/15/2022 12:00:00'),
+      startTime: new Date(today + ' 8:00:00'),
+      endTime: new Date(today + ' 12:00:00'),
     });
 
     draw.timeblock({
       col: 2,
       color: '#ff9c98',
-      startTime: new Date('06/15/2022 9:00:00'),
-      endTime: new Date('06/15/2022 13:20:00'),
+      startTime: new Date(today + ' 9:00:00'),
+      endTime: new Date(today + ' 13:20:00'),
     });
 
     draw.timeblock({
       col: 1,
       color: '#98b4ff',
-      startTime: new Date('06/15/2022 17:00:00'),
+      startTime: new Date(today + ' 17:00:00'),
       endTime: null,
     });
   }, []);
